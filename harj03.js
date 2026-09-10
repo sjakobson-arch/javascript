259983ae1437c687652e8b413b7e09e7f54d0171
Your Name <you@example.com>
2026-09-10 12:00:12 +0300

    rtr4ttr

---
+++ harj03.js
@@ -0,0 +1,18 @@
+const products = ["Õunad", "Piim", "Leib", "Juust", "Tomatid", "Kanafilee", "Muna", "Sibul", "Apelsinid", "Riis", "Jogurt", "Kartul", "Kalafilee", "Pasta", "Jogurtijook", "Porgandid", "Virsikud", "Pähklid", "Rosinad", "Kapsas", "Kreeka jogurt", "Veiseliha", "Banaanid", "Oliivid", "Mandlid", "Magus kartul", "Greibid"];
+
+
+
+//console.log(products[0]);
+//for
+for (let i = 0; i < products.length; i++) {
+    const prod = products[i];
+    console.log(prod);
+    
+}
+//while
+let j = 0
+
+while (j < products.length) {
+    console.log(products[j]);
+    j++
+}
